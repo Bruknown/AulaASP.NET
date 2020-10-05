e6859cf68aa8721ca43ed526667d3cdb5b10d246
Bruno Brandini Braz <brunobrandini77@gmail.com>
2020-10-05 11:44:46 -0300

    First Commit
    Adicionado entidades

---
+++ QuickBuy/QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Senha { get; set; }
+        public DateTime DataPedido {get; set;}
+        public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+
+        //pode haver pelo menos um pedido a vários
+        public ICollection<ItemPedido> itemPedido { get; set; }
+    }
+}
---
+++ QuickBuy/QuickBuy.Dominio/Entidades/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace QuickBuy.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int Senha { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal preco { get; set; }
+
+    }
+}
