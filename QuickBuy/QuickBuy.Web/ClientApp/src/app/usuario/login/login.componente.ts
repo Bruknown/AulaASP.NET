27553125161ae30b4e249d5718b6f0356857fe3f
Bruno Brandini Braz <brunobrandini77@gmail.com>
2020-10-07 11:35:49 -0300

    parte minima do front end feita
    começa aqui a parte que interliga o frontend com o backend

---
+++ QuickBuy/QuickBuy.Web/ClientApp/src/app/usuario/login/login.componente.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.componente.html",
+  styleUrls: ["./login.componente.css"]
+})
+export class LoginComponent {
+    
+}
